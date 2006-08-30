187d7448557eea8db6cab49c63383e25e5bb88b0
zebre007 <zebre007@ee5c1bf3-b648-4b9e-92f5-34e1ba6b1d6f>
2006-08-30 14:02:46 +0000

    added support for heterogeneous lists (xsd::all). validation using an internal schema must now be specified using xml::internal_grammar()

--- src/libraries/xeumeuleu/input_base_member.cpp
+++ src/libraries/xeumeuleu/input_base_member.cpp
@@ -78,18 +78,30 @@ DOMNode& input_base_member::parse( XERCES_CPP_NAMESPACE::InputSource& source, co
         Wrapper4InputSource input( &source, false );
         DOMImplementation* pImpl = DOMImplementationRegistry::getDOMImplementation( translate( "LS" ) );
         xerces_wrapper< DOMBuilder > pParser( ((DOMImplementationLS*)pImpl)->createDOMBuilder( DOMImplementationLS::MODE_SYNCHRONOUS, 0 ) );
+
         pParser->setFeature( XMLUni::fgXercesUserAdoptsDOMDocument, true );
         pParser->setFeature( XMLUni::fgDOMNamespaces, true );
         pParser->setFeature( XMLUni::fgDOMDatatypeNormalization, true );
         pParser->setFeature( XMLUni::fgXercesSchema, true );
         if( pGrammar )
         {
-            pParser->setFeature( XMLUni::fgDOMValidation, true );
-            pParser->setFeature( XMLUni::fgXercesUseCachedGrammarInParse, true );
-            pParser->loadGrammar( translate( *pGrammar ), Grammar::SchemaGrammarType, true ); // $$$$ MAT 2006-03-27: use pParser->setProperty( XMLUni::fgXercesSchemaExternalNoNameSpaceSchemaLocation, ... ) ?
+            const std::string schema = *pGrammar;
+            if( ! schema.empty() )
+            {
+                pParser->setFeature( XMLUni::fgDOMValidation, true );
+                pParser->setFeature( XMLUni::fgXercesUseCachedGrammarInParse, true );
+                // $$$$ MAT 2006-03-27: use pParser->setProperty( XMLUni::fgXercesSchemaExternalNoNameSpaceSchemaLocation, ... ) ?
+                pParser->loadGrammar( translate( *pGrammar ), Grammar::SchemaGrammarType, true );
+            }
+            else
+                pParser->setFeature( XMLUni::fgDOMValidateIfSchema, true );
         }
         else
+        {
             pParser->setFeature( XMLUni::fgDOMValidateIfSchema, true );
+            pParser->setFeature( XMLUni::fgDOMValidation, false );
+            pParser->setFeature( XMLUni::fgXercesLoadExternalDTD, false ) ;
+        }
         error_handler errorHandler;
         pParser->setErrorHandler( &errorHandler );
         DOMDocument* pDocument = pParser->parse( input );
