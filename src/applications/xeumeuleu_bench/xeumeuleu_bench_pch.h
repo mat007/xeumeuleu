7bf396301dd676a290df852f16a3f3a8713b29fc
Mathieu Champlon <mat007@users.noreply.github.com>
2007-05-28 08:16:35 +0000

    Added a benchmark application to ease the running of a profiler

---
+++ src/applications/xeumeuleu_bench/xeumeuleu_bench_pch.h
@@ -0,0 +1,18 @@
+// *****************************************************************************
+//
+// This file is part of a MASA library or program.
+// Refer to the included end-user license agreement for restrictions.
+//
+// Copyright (c) 2007 Mathématiques Appliquées SA (MASA)
+//
+// *****************************************************************************
+
+#ifndef __xeumeuleu_bench_pch_h_
+#define __xeumeuleu_bench_pch_h_
+
+#pragma warning( push, 0 )
+#include <boost/date_time/posix_time/posix_time.hpp>
+#pragma warning( pop )
+#include <iostream>
+
+#endif // __xeumeuleu_bench_pch_h_
