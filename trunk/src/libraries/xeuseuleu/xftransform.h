ee22474d620c65d9c0cbb6c64120a47bf07c5384
vinsle <vinsle@ee5c1bf3-b648-4b9e-92f5-34e1ba6b1d6f>
2007-09-21 09:24:44 +0000

    Merge from branch xslt revision 170 to trunk

---
+++ trunk/src/libraries/xeuseuleu/xftransform.h
@@ -0,0 +1,63 @@
+/*
+*   Copyright (c) 2007, Mathieu Champlon
+*   All rights reserved.
+*
+*   Redistribution  and use  in source  and binary  forms, with  or without
+*   modification, are permitted provided  that the following conditions are
+*   met :
+*
+*   . Redistributions  of  source  code  must  retain  the  above copyright
+*     notice, this list of conditions and the following disclaimer.
+*
+*   . Redistributions in  binary form  must reproduce  the above  copyright
+*     notice, this list of conditions  and the following disclaimer in  the
+*     documentation and/or other materials provided with the distribution.
+*
+*   . Neither  the name  of  the  copyright  holders  nor the names  of the
+*     contributors may be used to endorse  or promote products derived from
+*     this software without specific prior written permission.
+*
+*   THIS SOFTWARE  IS  PROVIDED  BY THE  COPYRIGHT HOLDERS  AND CONTRIBUTORS
+*   ``AS IS''  AND ANY  EXPRESS OR  IMPLIED WARRANTIES,  INCLUDING,  BUT NOT
+*   LIMITED TO, THE IMPLIED  WARRANTIES  OF MERCHANTABILITY AND  FITNESS FOR
+*   A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE THE COPYRIGHT
+*   OWNERS OR CONTRIBUTORS  BE LIABLE FOR ANY  DIRECT, INDIRECT, INCIDENTAL,
+*   SPECIAL,  EXEMPLARY,  OR  CONSEQUENTIAL   DAMAGES  (INCLUDING,  BUT  NOT
+*   LIMITED TO,  PROCUREMENT OF SUBSTITUTE  GOODS OR SERVICES;  LOSS OF USE,
+*   DATA, OR PROFITS; OR  BUSINESS INTERRUPTION) HOWEVER CAUSED  AND ON  ANY
+*   THEORY  OF  LIABILITY,  WHETHER IN  CONTRACT,  STRICT LIABILITY, OR TORT
+*   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY  WAY  OUT OF  THE USE
+*   OF THIS SOFTWARE, EVEN  IF  ADVISED OF  THE POSSIBILITY  OF SUCH DAMAGE.
+*/
+
+#ifndef _xeuseuleu_xftransform_h_
+#define _xeuseuleu_xftransform_h_
+
+#include "xf_base_member.h"
+#include "xtransform.h"
+
+namespace xsl
+{
+// =============================================================================
+/** @class  xftransform
+    @brief  Xsl file transform
+    @par    Using example
+    @code
+    xsl::xftransform xft( "transform.xsl", "output.xml" );
+    @endcode
+*/
+// Created: SLI 2007-09-07
+// =============================================================================
+class xftransform : private xf_base_member, public xtransform
+{
+public:
+    //! @name Constructors/Destructor
+    //@{
+             xftransform( const std::string& stylesheet, const std::string& filename );
+    virtual ~xftransform();
+    //@}
+};
+
+}
+
+#endif // _xeuseuleu_xftransform_h_
