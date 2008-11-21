89c57c32a2d721c340c329513663beb354201a31
mat007 <mat007@ee5c1bf3-b648-4b9e-92f5-34e1ba6b1d6f>
2008-11-21 23:30:15 +0000

    Cleanup

--- trunk/src/tests/xeuseuleu_test/xeuseuleu_test_pch.h
+++ trunk/src/tests/xeuseuleu_test/xeuseuleu_test_pch.h
@@ -30,8 +30,8 @@
  *   OF THIS SOFTWARE, EVEN  IF  ADVISED OF  THE POSSIBILITY  OF SUCH DAMAGE.
  */
 
-#ifndef __xeuseuleu_test_pch_h_
-#define __xeuseuleu_test_pch_h_
+#ifndef _xeuseuleu_test_pch_h_
+#define _xeuseuleu_test_pch_h_
 
 #ifdef _MSC_VER
 #   pragma warning( disable : 4355 )
@@ -62,4 +62,4 @@
 
 const std::string BOOST_RESOLVE( const std::string& filename );
 
-#endif // __xeuseuleu_test_pch_h_
+#endif // _xeuseuleu_test_pch_h_
