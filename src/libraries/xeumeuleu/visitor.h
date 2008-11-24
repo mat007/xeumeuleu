2bf97cbe7996552ff24602b173a74ec062434538
Mathieu Champlon <mat007@users.noreply.github.com>
2008-11-24 15:47:10 +0000

    Revert 470

--- src/libraries/xeumeuleu/visitor.h
+++ src/libraries/xeumeuleu/visitor.h
@@ -58,13 +58,6 @@ public:
     //@{
     virtual void process( const std::string& name, xistream& xis ) const = 0;
     //@}
-
-private:
-    //! @name Copy/Assignment
-    //@{
-    visitor( const visitor& );            //!< Copy constructor
-    visitor& operator=( const visitor& ); //!< Assignment operator
-    //@}
 };
 
 }
