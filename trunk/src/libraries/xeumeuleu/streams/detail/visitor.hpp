b391512ea9910e8d8535b2205ae0fd3f5f85cf3d
mat007 <mat007@ee5c1bf3-b648-4b9e-92f5-34e1ba6b1d6f>
2009-06-01 09:20:22 +0000

    Cleanup

--- trunk/src/libraries/xeumeuleu/streams/detail/visitor.hpp
+++ trunk/src/libraries/xeumeuleu/streams/detail/visitor.hpp
@@ -30,8 +30,8 @@
  *   OF THIS SOFTWARE, EVEN  IF  ADVISED OF  THE POSSIBILITY  OF SUCH DAMAGE.
  */
 
-#ifndef xeumeuleu_functor_hpp
-#define xeumeuleu_functor_hpp
+#ifndef xeumeuleu_visitor_hpp
+#define xeumeuleu_visitor_hpp
 
 #include <string>
 
@@ -62,4 +62,4 @@ public:
 
 }
 
-#endif // xeumeuleu_functor_hpp
+#endif // xeumeuleu_visitor_hpp
