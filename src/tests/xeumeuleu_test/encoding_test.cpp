acf8c5904b6d9c574eb1043ebb1167d977c70318
Mathieu Champlon <mat007@users.noreply.github.com>
2010-09-03 12:27:06 +0000

    Reworked tests on encodings

--- src/tests/xeumeuleu_test/encoding_test.cpp
+++ src/tests/xeumeuleu_test/encoding_test.cpp
@@ -34,110 +34,45 @@
 #include <xeumeuleu/xml.hpp>
 
 // -----------------------------------------------------------------------------
-// Name: creating_an_UTF_8_stream_with_non_UTF_8_characters_throws
-// Created: MCO 2006-01-03
+// Name: creating_an_input_stream_with_invalid_encoding_throws
+// Created: MCO 2010-09-03
 // -----------------------------------------------------------------------------
-BOOST_AUTO_TEST_CASE( creating_an_UTF_8_stream_with_non_UTF_8_characters_throws )
+BOOST_AUTO_TEST_CASE( creating_an_input_stream_with_invalid_encoding_throws )
 {
-    BOOST_CHECK_THROW( xml::xistringstream( "<?xml version='1.0' encoding='UTF-8' standalone='no' ?>"
-                                            "<element>ah ça c'est sûr !</element>" ), xml::exception );
+    BOOST_CHECK_THROW( xml::xistringstream( "<element/>", xml::encoding( "invalid_encoding" ) ), xml::exception );
 }
 
 // -----------------------------------------------------------------------------
-// Name: creating_an_UTF_16_stream_with_extended_characters_throws
-// Created: MCO 2006-01-03
+// Name: creating_an_output_stream_with_invalid_encoding_throws
+// Created: MCO 2010-09-03
 // -----------------------------------------------------------------------------
-BOOST_AUTO_TEST_CASE( creating_an_UTF_16_stream_with_extended_characters_throws )
+BOOST_AUTO_TEST_CASE( creating_an_output_stream_with_invalid_encoding_throws )
+{
+    BOOST_TODO;
+    //BOOST_CHECK_THROW( xml::xostringstream( xml::encoding( "invalid_encoding" ) ), xml::exception );
+}
+
+// -----------------------------------------------------------------------------
+// Name: forcing_encoding_upon_read_overrides_prolog_encoding
+// Created: MCO 2010-09-03
+// -----------------------------------------------------------------------------
+BOOST_AUTO_TEST_CASE( forcing_encoding_upon_read_overrides_prolog_encoding )
 {
     BOOST_CHECK_THROW( xml::xistringstream( "<?xml version='1.0' encoding='UTF-16' standalone='no' ?>"
-                                            "<element>ah ça c'est sûr !</element>" ), xml::exception );
+                                            "<element/>" ), xml::exception );
+    BOOST_CHECK_NO_THROW( xml::xistringstream( "<?xml version='1.0' encoding='UTF-16' standalone='no' ?>"
+                                               "<element/>", xml::encoding( "UTF-8" ) ) );
 }
 
 // -----------------------------------------------------------------------------
-// Name: creating_an_ISO_8859_1_stream_with_extended_characters_does_not_throw
-// Created: MCO 2006-01-03
+// Name: forcing_encoding_upon_write_overrides_prolog_encoding
+// Created: MCO 2010-09-03
 // -----------------------------------------------------------------------------
-BOOST_AUTO_TEST_CASE( creating_an_ISO_8859_1_stream_with_extended_characters_does_not_throw )
+BOOST_AUTO_TEST_CASE( forcing_encoding_upon_write_overrides_prolog_encoding )
 {
-    xml::xistringstream xis( "<?xml version='1.0' encoding='ISO-8859-1' standalone='no' ?>"
-                             "<element>ah ça c'est sûr !</element>" );
-    std::string value;
-    xis >> xml::content( "element", value );
-    BOOST_CHECK_EQUAL( "ah ça c'est sûr !", value );
-}
-
-// -----------------------------------------------------------------------------
-// Name: forcing_encoding_to_ISO_8859_1_allows_to_read_content
-// Created: MCO 2006-01-03
-// -----------------------------------------------------------------------------
-BOOST_AUTO_TEST_CASE( forcing_encoding_to_ISO_8859_1_allows_to_read_content )
-{
-    xml::xistringstream xis( "<element>ah ça c'est sûr !</element>", xml::encoding( "ISO-8859-1" ) );
-    std::string value;
-    xis >> xml::content( "element", value );
-    BOOST_CHECK_EQUAL( "ah ça c'est sûr !", value );
-}
-
-// -----------------------------------------------------------------------------
-// Name: forcing_encoding_to_ISO_8859_1_whereas_UTF_8_is_specified_in_document_allows_to_read_content
-// Created: MCO 2006-01-03
-// -----------------------------------------------------------------------------
-BOOST_AUTO_TEST_CASE( forcing_encoding_to_ISO_8859_1_whereas_UTF_8_is_specified_in_document_allows_to_read_content )
-{
-    xml::xistringstream xis( "<?xml version='1.0' encoding='UTF-8' standalone='no' ?>"
-                             "<element>ah ça c'est sûr !</element>", xml::encoding( "ISO-8859-1" ) );
-    std::string value;
-    xis >> xml::content( "element", value );
-    BOOST_CHECK_EQUAL( "ah ça c'est sûr !", value );
-}
-
-// -----------------------------------------------------------------------------
-// Name: creating_a_stream_with_invalid_encoding_throws
-// Created: MCO 2006-01-03
-// -----------------------------------------------------------------------------
-BOOST_AUTO_TEST_CASE( creating_a_stream_with_invalid_encoding_throws )
-{
-    BOOST_TODO; // $$$$ MAT 2006-01-09: not as easy as it seems
-//    BOOST_CHECK_THROW( xml::xostringstream( "invalid_encoding" ), xml::exception );
-}
-
-// -----------------------------------------------------------------------------
-// Name: streaming_extended_character_into_ISO_8859_1_makes_a_valid_document
-// Created: MCO 2006-01-03
-// -----------------------------------------------------------------------------
-BOOST_AUTO_TEST_CASE( streaming_extended_character_into_ISO_8859_1_makes_a_valid_document )
-{
-    // warning : boost logs by streaming into std::ostream therefore the message can be malformed
-    xml::xostringstream xos( xml::encoding( "ISO-8859-1" ) );
-    xos << xml::content( "element", "ah ça c'est sûr !" );
-    BOOST_CHECK_EQUAL( "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" standalone=\"no\" ?>\n"
-                       "<element>ah ça c'est sûr !</element>\n", xos.str() );
-}
-
-// -----------------------------------------------------------------------------
-// Name: streaming_extended_character_into_UTF_16_makes_a_valid_document
-// Created: MCO 2006-01-03
-// -----------------------------------------------------------------------------
-BOOST_AUTO_TEST_CASE( streaming_extended_character_into_UTF_16_makes_a_valid_document )
-{
-    BOOST_TODO; // $$$$ MAT 2006-04-23: must think about adding support for std::wstring ?
-//    xml::xowstringstream xos( xml::encoding( "UTF-16" ) );
-//    xos << xml::content( "element", "ah ça c'est sûr !" );
-//    BOOST_CHECK_EQUAL( "<?xml version=\"1.0\" encoding=\"UTF-16\" standalone=\"no\" ?>\n"
-//                       "<element>ah ça c'est sûr !</element>\n", xos.str() );
-}
-
-// -----------------------------------------------------------------------------
-// Name: streaming_extended_character_into_UTF_8_makes_a_valid_document
-// Created: MCO 2006-01-03
-// -----------------------------------------------------------------------------
-BOOST_AUTO_TEST_CASE( streaming_extended_character_into_UTF_8_makes_a_valid_document )
-{
-    const std::string content( "ah ça c'est sûr !" );
-    xml::xostringstream xos( xml::encoding( "UTF-8" ) );
-    xos << xml::content( "element", content );
-    xml::xistringstream xis( xos.str() );
-    std::string value;
-    xis >> xml::content( "element", value );
-    BOOST_CHECK_EQUAL( content, value );
+    xml::xostringstream xos1( xml::encoding( "UTF-8" ) );
+    xos1 << xml::start( "element" );
+    xml::xostringstream xos2( xml::encoding( "UTF-16" ) );
+    xos2 << xml::start( "element" );
+    BOOST_CHECK_NE( xos1.str(), xos2.str() );
 }
