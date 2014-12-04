3c64f1460976dd634d8755e259c97d776c13c84b
Mathieu Champlon <mat007@users.noreply.github.com>
2014-12-04 22:04:25 +0000

    Removed line and column from context when they are 0, 0

--- src/tests/xeumeuleu_test/context_test.cpp
+++ src/tests/xeumeuleu_test/context_test.cpp
@@ -40,7 +40,7 @@
 BOOST_AUTO_TEST_CASE( root_has_context )
 {
     xml::xistringstream xis( "<root/>" );
-    BOOST_CHECK_EQUAL( "string_input (line 0, column 0) : ", xis.context() );
+    BOOST_CHECK_EQUAL( "string_input : ", xis.context() );
 }
 
 // -----------------------------------------------------------------------------
